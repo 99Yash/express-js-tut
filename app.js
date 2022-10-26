657045a6301f1d7088b17f9cd287fa1e0a727ca3
yash <yashgouravkar@gmail.com>
2022-10-26 17:30:20 +0530

    upto datetill  module 6 minus assignments

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   console.log('In the middleware!');
-//   next(); // Allows the request to continue to the next middleware in line
-// });
-// app.use((req, res, next) => {
-//   console.log('In the 2nd middleware!');
-//res.send('<h1>Hello from Express!</h1>');
-// });
-
-// app.use() is a method that allows us to register a new middleware for all types of incoming requests
-//app.get() is a method that allows us to register a new middleware for incoming GET requests
-//app.post() is a method that allows us to register a new middleware for incoming POST requests
-
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,22 @@
 const path = require('path');
 
-const rootDir = require('../utils/path');
-
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // __dirname refers to the directory where this file is located(i.e the route folder)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
