d4a5d1a7a82181cc1a5dc55cabd841a8afa55e2a
yash <yashgouravkar@gmail.com>
2022-10-29 09:12:37 +0530

    refactor: initialised products json to []

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/404');
 
 const app = express();
 
--- controllers/404.js
+++ controllers/404.js
@@ -1,3 +1,3 @@
 exports.send404 = (req, res) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
