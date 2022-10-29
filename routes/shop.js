4a2f1664bf5d5cc17ea2f5709671435f4f5a617a
yash <yashgouravkar@gmail.com>
2022-10-29 09:47:07 +0530

    refactor: deleted all pug & hbs files

--- routes/shop.js
+++ routes/shop.js
@@ -12,6 +12,8 @@ router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
+router.post('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
