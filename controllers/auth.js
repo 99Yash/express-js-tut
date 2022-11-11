60a79a2b3e334907c66b23f057c11d718f54b222
yash <yashgouravkar@gmail.com>
2022-11-11 15:52:51 +0530

    feat: added signup validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator/check');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -75,22 +76,25 @@ exports.getSignup = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        //this means that the user already exists
-        req.flash('error', 'Email already exists.');
-        return res.redirect('/login');
-      }
-      return bcrypt.hash(password, 12).then((hashedPassword) => {
-        //create a new user
-        const user = new User({
-          email: email,
-          password: password,
-          cart: { items: [] },
-        });
-        return user.save();
+
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      //create a new user
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
       });
+      return user.save();
     })
     .then((result) => {
       res.redirect('/login');
